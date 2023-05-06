c10fadd2155bc8d3bfb33259468823a2cecd7037
sanskar021 <sanskarchaurey2000@gmail.com>
2023-05-06 10:03:55 +0530

    todo list

---
+++ src/components/basic.js
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+// const basic = () => {
+//   return (
+//     <div>
+//    {/* this word type "rafce - react arrow function and components" */}
+//      </div>
+//   )
+// }
+
+// export default basic
